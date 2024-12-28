2096d0143eaab995f2e73bfe46b0f7bcf1eb2902
officeyutong <yt.xyxx@gmail.com>
2024-12-28 20:56:18 +0800

    able to load light-client library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
-// import networkConfig from "../config.toml";
+import networkConfig from "./config.toml";
 import LightClient, { randomSecretKey } from "light-client-js";
 
 const Main: React.FC<{}> = () => {
     const [loading, setLoading] = useState(false);
     const [client, setClient] = useState<null | LightClient>(null);
-
     useEffect(() => {
         if (client === null) (async () => {
             try {
                 setLoading(true);
+                const config = await (await fetch(networkConfig)).text();
                 const client = new LightClient();
-                await client.start({ type: "TestNet", config: 'ssss' }, randomSecretKey(), "info");
+                await client.start({ type: "TestNet", config }, randomSecretKey(), "info");
                 setClient(client);
                 setLoading(false);
             } catch (e) { console.error(e); }
         })();
     }, [client]);
     return <div style={{ marginTop: "10%", marginLeft: "10%", marginRight: "10%" }}>
-        {loading && "loading.."}
+        {loading && "loading..11"}
     </div>
 }
 
